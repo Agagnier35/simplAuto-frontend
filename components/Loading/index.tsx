12681801dfe22c8479444f5a32891d0c4f0aab92
Mohammed Tigrini <mtigrini@gmail.com>
2019-02-09 11:36:17 -0500

    Ajout du component IsNotLoggedIn et affichage du nom de l'utilisateur dans le header lorsque celui-ci est connecté.

---
+++ components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { multi, MultiProps } from '../../lib/MultiLang';
+import StyledLoading from './styles';
+
+const loading = ({ translations }: MultiProps) => {
+  return (
+    <StyledLoading>
+      <p>{translations.loading}</p>
+    </StyledLoading>
+  );
+};
+
+export default multi(loading);
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,8 @@ const Home = ({
       >
         {general.changeLangage}
       </button>
-      <Login />
-      <Signup/>
+      {/* <Login />
+      <Signup/> */}
     </div>
   );
 };
