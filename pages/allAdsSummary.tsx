d33e8f3c3a0fb717cdabd1322df0602a2f5d8db5
william chartier <chartier_william@outlook.fr>
2019-02-11 13:16:17 -0500

    re arrangement des fichiers et des components

---
+++ pages/allAdsSummary.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { multi, MultiProps } from '../lib/MultiLang';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import IsLoggedIn from '../components/IsLoggedIn';
+import ErrorMessage from "../components/ErrorMessage";
+import Loading from '../components/Loading';
+import GenerateAllAd from '../components/AllAds'
+
+const Ads = ({ translations }: MultiProps) => {
+
+    const ALL_ADS_QUERY = gql` 
+    query{
+        ads{
+            id
+            priceLowerBoundFeature{price}
+            priceHigherBoundFeature{price}
+            manufacturerFeature{manufacturer{name}}
+            modelFeature{model{name}}
+            categoryFeature{category{name}}
+            mileageLowerBoundFeature{mileage}
+            mileageHigherBoundFeature{mileage}
+            yearLowerBoundFeature{year}
+            yearHigherBoundFeature{year}
+            features{feature{name,category{name}}}
+            isUrgent
+            isFirst
+            status
+            }
+        }
+    `;
+
+  return(
+        <Query query={ALL_ADS_QUERY} >
+        {({ data, loading, error }) => {
+            if (loading) return <Loading></Loading>;
+            if (error) return <ErrorMessage></ErrorMessage>;
+            return data.ads.map((currentAd : any) => (
+                <GenerateAllAd key={currentAd.id} data={currentAd} />
+            ));
+        }}
+        </Query>
+  );
+};
+
+export default multi(Ads);
