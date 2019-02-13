41a0cf48bc9a019fc58e5a284ecb892b46b1eed1
Unknown <mtigrini@gmail.com>
2019-02-13 14:01:35 -0500

    Decommenting de la redirection dans le isLoggedIn

--- components/IsLoggedIn/index.tsx
+++ components/IsLoggedIn/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import gql from 'graphql-tag';
-///import Router from 'next/router';
+import Router from 'next/router';
 import { Query } from 'react-apollo';
 import Loading from '../Loading';
 
@@ -20,7 +20,7 @@ const IsLoggedIn = ({ children }: { children: React.ReactNode }) => {
           return <Loading />;
         }
         if (error) {
-          // Router.push('/');
+          Router.push('/');
           return null;
         } else {
           return children;
