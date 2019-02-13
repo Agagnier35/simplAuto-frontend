bc1cd5dae6789c9340416e68d230c1abfe1e14db
Unknown <mtigrini@gmail.com>
2019-02-13 15:19:25 -0500

    Supression de la fonction toShow et mise du jsx à l'intérieur de render.

--- components/resetPw/index.tsx
+++ components/resetPw/index.tsx
@@ -48,7 +48,7 @@ class ResetPw extends Component<MultiProps, ResetPwState> {
 
   handleResetPassword = async (e: FormEvent<any>, resetPassword: () => void) => {
     e.preventDefault();
-    this.isResetPasswordValid() ? await resetPassword() : this.setState({ newPassword: '' });s
+    this.isResetPasswordValid() ? await resetPassword() : this.setState({ newPassword: '' });
     this.setState({ newPassword: '' });
   };
 
@@ -61,106 +61,104 @@ class ResetPw extends Component<MultiProps, ResetPwState> {
     this.setState({ [e.currentTarget.name]: e.currentTarget.value } as any);
   };
 
-  toShow = (general: any) => {
-    if (this.props.query.resetToken) {
-      return (  <Mutation mutation={RESET_PW_MUTATION} variables={{resetToken: this.props.query.resetToken,
-                                                                  password: this.state.newPassword}}>
-        {(handleMutation, { loading, error }) => (
-          <StyledLogin>
-          <Card>
-            <BrandHeader />
-            <Card.Body>
-              <Form
-                method="post"
-                onSubmit={(e: any) => this.handleResetPassword(e, handleMutation)}>
-                <fieldset disabled={loading} aria-busy={loading}>
-                    <Form.Group>
-                      <Form.Label>{general.password}</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Prepend>
-                          <InputGroup.Text id="inputGroupPrepend">
-                          <MdLockOutline />
-                          </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                          aria-describedby="inputGroupPrepend"
-                          required
-                          type="password"
-                          name="newPassword"
-                          placeholder={general.password}
-                          value={this.state.newPassword}
-                          onChange={this.handleChange}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          {general.password}
-                        </Form.Control.Feedback>
-                      </InputGroup>
-                    </Form.Group>
-                    <Button variant="primary" type="submit" block>
-                      {general.resetPw}
-                    </Button>
-                </fieldset>
-              </Form>
-            </Card.Body>
-          </Card>
-          <ErrorMessage error={error} />
-        </StyledLogin>
-        )}
-          </Mutation>)
-    } else {
-      return (
-        <Mutation mutation={RESET_PW_REQUEST_MUTATION} variables={{email: this.state.email}}>
-          {(handleMutation, { loading, error }) => (
-           <StyledLogin>
-           <Card>
-             <BrandHeader />
-             <Card.Body>
-               <Form
-                 method="post"
-                 onSubmit={(e: any) => this.handleResetPasswordRequest(e, handleMutation)}>
-                 <fieldset disabled={loading} aria-busy={loading}>
-                 <Form.Group>
-                      <Form.Label>{general.email}</Form.Label>
-                      <InputGroup>
-                        <InputGroup.Prepend>
-                          <InputGroup.Text id="inputGroupPrepend">
-                            @
-                          </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                          placeholder={general.email}
-                          aria-describedby="inputGroupPrepend"
-                          required
-                          type="email"
-                          name="email"
-                          value={this.state.email}
-                          onChange={this.handleChange}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          {general.email}
-                        </Form.Control.Feedback>
-                      </InputGroup>
-                    </Form.Group>
-                     <Button variant="primary" type="submit" block>
-                       {general.resetPw}
-                     </Button>
-                 </fieldset>
-               </Form>
-             </Card.Body>
-           </Card>
-           <ErrorMessage error={error} />
-         </StyledLogin>)}
-            </Mutation>
-      );
-    }
-  }
-
   render() {
     const {
       translations: { general },
     } = this.props;
 
-    return this.toShow(general);
+    return  () => {
+      if (this.props.query.resetToken) {
+        return (  <Mutation mutation={RESET_PW_MUTATION} variables={{resetToken: this.props.query.resetToken,
+                                                                    password: this.state.newPassword}}>
+          {(handleMutation, { loading, error }) => (
+            <StyledLogin>
+            <Card>
+              <BrandHeader />
+              <Card.Body>
+                <Form
+                  method="post"
+                  onSubmit={(e: any) => this.handleResetPassword(e, handleMutation)}>
+                  <fieldset disabled={loading} aria-busy={loading}>
+                      <Form.Group>
+                        <Form.Label>{general.password}</Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">
+                            <MdLockOutline />
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            aria-describedby="inputGroupPrepend"
+                            required
+                            type="password"
+                            name="newPassword"
+                            placeholder={general.password}
+                            value={this.state.newPassword}
+                            onChange={this.handleChange}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {general.password}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+                      <Button variant="primary" type="submit" block>
+                        {general.resetPw}
+                      </Button>
+                  </fieldset>
+                </Form>
+              </Card.Body>
+            </Card>
+            <ErrorMessage error={error} />
+          </StyledLogin>
+          )}
+            </Mutation>)
+      } else {
+        return (
+          <Mutation mutation={RESET_PW_REQUEST_MUTATION} variables={{email: this.state.email}}>
+            {(handleMutation, { loading, error }) => (
+             <StyledLogin>
+             <Card>
+               <BrandHeader />
+               <Card.Body>
+                 <Form
+                   method="post"
+                   onSubmit={(e: any) => this.handleResetPasswordRequest(e, handleMutation)}>
+                   <fieldset disabled={loading} aria-busy={loading}>
+                   <Form.Group>
+                        <Form.Label>{general.email}</Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">
+                              @
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            placeholder={general.email}
+                            aria-describedby="inputGroupPrepend"
+                            required
+                            type="email"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {general.email}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+                       <Button variant="primary" type="submit" block>
+                         {general.resetPw}
+                       </Button>
+                   </fieldset>
+                 </Form>
+               </Card.Body>
+             </Card>
+             <ErrorMessage error={error} />
+           </StyledLogin>)}
+              </Mutation>
+        );
+      }
+    };
   }
 }
 
