9e227718b650decb7371f86f2a0d1a1034a32e5f
Alexandre Clark <alex.clark.015@gmail.com>
2019-02-13 00:42:18 -0500

    quick fix

--- components/CarAdd/CheckboxFeatures.tsx
+++ components/CarAdd/CheckboxFeatures.tsx
@@ -7,44 +7,50 @@ import { multi, MultiProps } from '../../lib/MultiLang';
 import { Table } from 'react-bootstrap';
 
 interface CheckboxFeatureProps {
-  handleChange: any
-  features: any
+  handleChange: any;
+  features: any;
 }
 
 const GET_FEATURES = gql`
-query{
-    carFeatureCategories{
+  query {
+    carFeatureCategories {
+      id
+      name
+      type
+      features {
         id
         name
-        type
-        features {
-            id
-            name
-        }
-
+      }
     }
-}`;
+  }
+`;
 
 const CheckboxFeatures = ({
   handleChange,
   features,
   translations: { carFeatureCategory },
 }: MultiProps & CheckboxFeatureProps) => (
-    <Table >
-      {() => {
-        return (
-          <div>
-            {features.map((featureCategory: any) => (
-              <tr>
-                <td>{carFeatureCategory[featureCategory.name]}</td>
-                <td>
-                  <input type="checkbox" name={featureCategory.features[0].id} value={featureCategory.features[0].id}
-                    onChange={(e) => handleChange('features', { value: e.currentTarget.value, category: featureCategory.features[0].name })}></input>
-                </td>
-              </tr>
-            ))}
-          </div>);
-      }}
-    </Table>
-  );
+  <Table>
+    <div>
+      {features.map((featureCategory: any) => (
+        <tr>
+          <td>{carFeatureCategory[featureCategory.name]}</td>
+          <td>
+            <input
+              type="checkbox"
+              name={featureCategory.features[0].id}
+              value={featureCategory.features[0].id}
+              onChange={e =>
+                handleChange('features', {
+                  value: e.currentTarget.value,
+                  category: featureCategory.features[0].name,
+                })
+              }
+            />
+          </td>
+        </tr>
+      ))}
+    </div>
+  </Table>
+);
 export default multi(CheckboxFeatures);
--- components/CarAdd/DropdownFeatures.tsx
+++ components/CarAdd/DropdownFeatures.tsx
@@ -7,50 +7,57 @@ import { multi, MultiProps } from '../../lib/MultiLang';
 import { Table } from 'react-bootstrap';
 
 interface DropboxFeatureProps {
-  handleChange: any
-  features: any
+  handleChange: any;
+  features: any;
 }
 
 const GET_FEATURES = gql`
-query{
-    carFeatureCategories{
+  query {
+    carFeatureCategories {
+      id
+      name
+      type
+      features {
         id
         name
-        type
-        features {
-            id
-            name
-        }
-
+      }
     }
-}`;
+  }
+`;
 
 const DropDownFeatures = ({
   handleChange,
   features,
   translations: { general, carFeatureCategory },
 }: MultiProps & DropboxFeatureProps) => (
-    <Table>
-      {() => {
-        return (
-          <div>
-            {features.map((category: any) => (
-              <tr>
-                <td>{carFeatureCategory[category.name]}</td>
-                <td>
-                  <select onChange={(e) => handleChange('features', { value: e.currentTarget.value, category: category.name })}>
-                    <option disabled selected hidden>{general.defaultDropdown}</option>
-                    {category.features.map((feature: any) =>
-                      <option key={feature.id} value={feature.id}>
-                        {feature.name}
-                      </option>)}
-                    <option>{general.none}</option>
-                  </select>
-                </td>
-              </tr>
-            ))}
-          </div>);
-      }}
-    </Table>
-  );
+  <Table>
+    <div>
+      {features.map((category: any) => (
+        <tr>
+          <td>{carFeatureCategory[category.name]}</td>
+          <td>
+            <select
+              onChange={e =>
+                handleChange('features', {
+                  value: e.currentTarget.value,
+                  category: category.name,
+                })
+              }
+            >
+              <option disabled selected hidden>
+                {general.defaultDropdown}
+              </option>
+              {category.features.map((feature: any) => (
+                <option key={feature.id} value={feature.id}>
+                  {feature.name}
+                </option>
+              ))}
+              <option>{general.none}</option>
+            </select>
+          </td>
+        </tr>
+      ))}
+    </div>
+  </Table>
+);
 export default multi(DropDownFeatures);
