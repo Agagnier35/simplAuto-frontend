906a01cb9147b1954a78495e226f00f726436b08
Unknown <mtigrini@gmail.com>
2019-02-18 11:47:36 -0500

    Modification du client id pour Google login

--- components/Login/GoogleLogin/index.tsx
+++ components/Login/GoogleLogin/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import GoogleLogin from "react-google-login";
 
-function responseGoogle() {
-    console.log("Here.");
+function responseGoogle(response: any) {
+    console.log(response);
 }
 
 
 const googleLogin = () => {
     return ( <GoogleLogin
-        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-        buttonText="Login"
+        clientId="336221605230-o9967qlvh1121ublhs76bh9flm6kb3r8.apps.googleusercontent.com"
+        buttonText="Login With Google"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
       />);
