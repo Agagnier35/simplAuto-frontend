50cfa9abc0685018927db764b9175dc736879e43
Unknown <mtigrini@gmail.com>
2019-02-18 12:30:01 -0500

    Login or Signup à partir de google.

--- components/Login/FacebookLogin/index.tsx
+++ components/Login/FacebookLogin/index.tsx
@@ -1,22 +1,29 @@
 import FacebookLogin from "react-facebook-login";
 import * as React from 'react';
 
-function responseFacebook(response: any){
-    console.log(response);
+function responseFacebook(response: any) {
+    loginOrSignup(response);
 }
 
-function componentClicked() {
-    console.log("clicked");
+const loginOrSignup = (response: any) => { 
+    console.log(response);
+    // Vérifier que le facebook user id ne se trouve pas dans la database.
+    //      S'il se trouve dans la database, login()
+    //      S'il ne se trouve pas dans la database, signup()
+}
+
+function componentClicked() { 
+    console.log("clicked"); 
 }
 
 const facebookLogin = () => {
     return (<div>
         <FacebookLogin
-        appId="1017021355164596"
-        autoLoad={true}
-        fields="first_name,name,last_name,email,gender,birthday,picture"
-        onClick={componentClicked}
-        callback={responseFacebook} />
+            appId="1017021355164596"
+            autoLoad={true}
+            fields="first_name,name,last_name,email,gender,birthday,picture"
+            onClick={componentClicked}
+            callback={responseFacebook} />
         </div>)
 }
 
