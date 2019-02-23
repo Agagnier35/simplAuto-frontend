4d779b4f6de59bd5a36794add134ad50cd40d200
Unknown <mtigrini@gmail.com>
2019-02-23 18:01:01 -0500

    Enlèvement d'un div pour la connexion via facebook.

--- components/Login/FacebookLogin/index.tsx
+++ components/Login/FacebookLogin/index.tsx
@@ -18,15 +18,13 @@ function componentClicked() {
 
 const facebookLogin = () => {
   return (
-    <div>
-      <FacebookLogin
-        appId="1017021355164596"
-        autoLoad={true}
-        fields="first_name,name,last_name,email,gender,birthday,picture"
-        onClick={componentClicked}
-        callback={responseFacebook}
-      />
-    </div>
+    <FacebookLogin
+      appId="1017021355164596"
+      autoLoad={true}
+      fields="first_name,name,last_name,email,gender,birthday,picture"
+      onClick={componentClicked}
+      callback={responseFacebook}
+    />
   );
 };
 
