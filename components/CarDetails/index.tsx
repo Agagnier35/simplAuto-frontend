03f1798e0c1590245ac1ab83c843c018004222e6
Unknown <marc-antoine.therrien@polymtl.ca>
2019-02-23 16:26:37 -0500

    un peu de style, mais pas vrm beau

--- components/CarDetails/index.tsx
+++ components/CarDetails/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Translations from '../../lib/MultiLang/locales/types';
 import StyledCarDetails from './styles';
 import { multi } from '../../lib/MultiLang';
-import { Carousel, ListGroup } from 'react-bootstrap';
+import { Carousel, ListGroup, Card } from 'react-bootstrap';
 import { Car } from '../../generated/graphql';
 
 export interface CarDetailsProps {
@@ -10,7 +10,14 @@ export interface CarDetailsProps {
   car: Car;
 }
 const CarDetails = ({ translations, car }: CarDetailsProps) => {
-  const { cars, carCategory, carFeatureCategory, carFeature } = translations;
+  const {
+    cars,
+    carCategory,
+    carFeatureCategory,
+    carFeature,
+    carLabel,
+    general,
+  } = translations;
   return (
     <StyledCarDetails>
       <div className="carouselSection">
@@ -34,29 +41,57 @@ const CarDetails = ({ translations, car }: CarDetailsProps) => {
         </Carousel>
       </div>
 
-      <ListGroup>
-        <ListGroup.Item>
-          {cars.manufacturer}: {car.manufacturer.name}
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {cars.model}: {car.model.name}
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {cars.category}: {carCategory[car.category.name] || car.category.name}
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {cars.year}: {car.year}
-        </ListGroup.Item>
-        <ListGroup.Item>
-          {cars.mileage}: {car.mileage}
-        </ListGroup.Item>
-        {car.features.map((f: any) => (
-          <ListGroup.Item>
-            {carFeatureCategory[f.category.name]}:{' '}
-            {carFeature[f.name] || f.name}
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      <div className="card-wrapper">
+        <div className="firstCardWrapper">
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                <span className="card-number">1</span>
+                {carLabel.general}
+              </Card.Title>
+              <label>
+                <ListGroup>
+                  <ListGroup.Item>
+                    {cars.manufacturer}: {car.manufacturer.name}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {cars.model}: {car.model.name}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {cars.category}:{' '}
+                    {carCategory[car.category.name] || car.category.name}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {cars.year}: {car.year}
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    {cars.mileage}: {car.mileage}
+                  </ListGroup.Item>
+                </ListGroup>
+              </label>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <span className="card-number">2</span>
+              {general.features}
+            </Card.Title>
+            <label>
+              <ListGroup>
+                {car.features.map((f: any) => (
+                  <ListGroup.Item>
+                    {carFeatureCategory[f.category.name]}:{' '}
+                    {carFeature[f.name] || f.name}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </label>
+          </Card.Body>
+        </Card>
+      </div>
     </StyledCarDetails>
   );
 };
--- components/CarDetails/styles.ts
+++ components/CarDetails/styles.ts
@@ -14,6 +14,32 @@ const StyledCarDetails = styled.div`
     padding: auto;
     max-height: 400px;
   }
+
+  .card-number {
+    color: ${props => props.theme.colors.primary};
+    font-size: 2.5rem;
+    line-height: 1.4rem;
+    margin-right: 1rem;
+  }
+
+  .firstCardWrapper {
+    margin-bottom: 15px;
+  }
+
+  .card-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -0.5rem;
+
+    .card {
+      margin: 0.5rem;
+      flex-grow: 1;
+
+      & + .card {
+        flex-grow: 10;
+      }
+    }
+  }
 `;
 
 export default StyledCarDetails;
