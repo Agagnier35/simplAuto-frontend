912f66d256cdb7bbb7349d76f8de84b9f668087e
Unknown <mtigrini@gmail.com>
2019-02-24 17:20:44 -0500

    Suppression des commentaires et imports inutiles.

--- components/Login/FacebookLogin/index.tsx
+++ components/Login/FacebookLogin/index.tsx
@@ -1,7 +1,7 @@
 import FacebookLogin from 'react-facebook-login';
 import { Mutation } from 'react-apollo';
 import { LOGGED_IN_QUERY } from '../../IsLoggedIn';
-import React, { FormEvent, Component } from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { multi, MultiProps } from '../../../lib/MultiLang';
 
@@ -13,49 +13,8 @@ const FACEBOOK_LOGIN_MUTATION = gql`
   }
 `;
 
-// function responseFacebook(response: any) {
-//   loginOrSignup(response);
-// }
-
-const loginOrSignup = (response: any) => {
-  console.log(response);
-  // Vérifier que le facebook user id ne se trouve pas dans la database.
-  // Si la réponse est valide
-  //    S'il se trouve dans la database, login()
-  //    S'il ne se trouve pas dans la database, signup()
-  // Si la réponse est invalide
-  //    Envoyer un message comme quoi la connexion facebook a échoué
-};
-
-// function componentClicked() {
-//   console.log('clicked');
-// }
-
-// const facebookLogin = () => {
-//   return (
-//     <Mutation
-//       mutation={FACEBOOK_LOGIN_MUTATION}
-//       variables={null}
-//       refetchQueries={[{ query: LOGGED_IN_QUERY }]}
-//     >  {() => (
-//       <FacebookLogin
-//         appId="1017021355164596"
-//         autoLoad={true}
-//         fields="first_name,name,last_name,email,gender,birthday,picture"
-//         onClick={componentClicked}
-//         callback={responseFacebook}
-//       />)}
-//     </Mutation>
-//   );
-// };
-
 class facebookLogin extends Component<MultiProps> {
   state = {};
-
-  componentClicked = async (facebookLogin: () => void) => {
-    console.log('Hello');
-    // await facebookLogin();
-  };
 
   responseFacebook = (response: any) => {
     console.log(response);
