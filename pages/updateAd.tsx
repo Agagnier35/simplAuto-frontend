73919940453b456d8d75a2dc1fde7d0ac75718b4
Unknown <mtigrini@gmail.com>
2019-02-25 12:34:10 -0500

    Passage du adId par queryString au component updateAd.

--- pages/updateAd.tsx
+++ pages/updateAd.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import UpdateAd from '../components/UpdateAd';
+import IsLoggedIn from '../components/IsLoggedIn';
+import Translations from '../lib/MultiLang/locales/types';
 
-const UpdateAdPage = () => {
-  return <UpdateAd />;
+export interface AdProps {
+  translations: Translations;
+  query: {
+    adId: string;
+  };
+}
+
+const UpdateAdPage = ({ query }: AdProps) => {
+  return (
+    <IsLoggedIn>
+      <UpdateAd adId={query.adId} />
+    </IsLoggedIn>
+  );
 };
 
 export default UpdateAdPage;
