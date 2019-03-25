9d79a645b4d0a8345279bd5db88f30c2cded8597
Mohammed Tigrini <mtigrini@gmail.com>
2019-03-25 04:47:11 -0400

    Query pour get les messages.

--- pages/messages.tsx
+++ pages/messages.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import Translations from '../lib/MultiLang/locales/types';
 import PrivateComponent from '../lib/Auth/PrivateComponent';
 
-// export interface CarPageProps {
-//   translations: Translations;
-//   query: {
-//     resetToken: string;
-//   };
-// }
-
 class Messages extends PrivateComponent {
   render() {
     return <div>Messages</div>;
