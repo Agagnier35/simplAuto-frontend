61a7a74240757c9f70f0a31f65d4bb8759a06dee
Alexandre Clark <alex.clark.015@gmail.com>
2019-03-27 20:40:32 -0400

    admin page done only for admin

---
+++ components/Admin/Admin/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface AdminPageProps {}
+
+const AdminPage = (props: AdminPageProps) => {
+  return <div>admin</div>;
+};
+
+export default AdminPage;
--- components/General/Header/index.tsx
+++ components/General/Header/index.tsx
@@ -10,7 +10,7 @@ import { IoMdCar } from 'react-icons/io';
 import { Query, Mutation } from 'react-apollo';
 import Notifications from '../Notifications';
 import CommonDataManager, { appName } from '../Preferences';
-import { ClientType } from '../../../generated/graphql';
+import { ClientType, Permission } from '../../../generated/graphql';
 
 export const LOGGED_IN_QUERY = gql`
   query LOGGED_IN_QUERY {
@@ -21,6 +21,7 @@ export const LOGGED_IN_QUERY = gql`
       companyName
       clientType
       email
+      permissions
       notifications {
         id
         type
@@ -82,9 +83,15 @@ const Header: React.SFC<MultiProps> = ({
                       <Link href="/cars" passHref prefetch>
                         <Nav.Item as="a">{general.sell}</Nav.Item>
                       </Link>
-                      <Link href="/premium" passHref prefetch>
-                        <Nav.Item as="a">Premium</Nav.Item>
-                      </Link>
+                      {data.me.permissions.includes(Permission.Admin) ? (
+                        <Link href="/admin" passHref prefetch>
+                          <Nav.Item as="a">Admin</Nav.Item>
+                        </Link>
+                      ) : (
+                        <Link href="/premium" passHref prefetch>
+                          <Nav.Item as="a">Premium</Nav.Item>
+                        </Link>
+                      )}
                       <Link href="/profile" passHref>
                         <a className="firstName">
                           {data.me.clientType === ClientType.Individual
