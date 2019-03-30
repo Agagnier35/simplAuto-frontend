1d348540d3234af796eb23c6e2ffcec7238f1d3e
The Savior <davidd343@hotmail.com>
2019-03-30 17:36:02 -0400

    conversation refresh

--- components/Offer/Offer/Mutations.ts
+++ components/Offer/Offer/Mutations.ts
@@ -4,6 +4,7 @@ export const CREATE_CONVERSATION_MUTATION = gql`
   mutation CREATE_CONVERSATION_MUTATION($offerID: ID!) {
     createConversation(offerID: $offerID) {
       id
+      status
     }
   }
 `;
