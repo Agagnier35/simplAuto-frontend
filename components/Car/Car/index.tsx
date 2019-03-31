fa0b1c7a33176b9de1984219f9583f2084723a07
Marc-Antoine <marc-antoine.therrien@polymtl.ca>
2019-03-30 22:14:46 -0400

    fix comportement

--- components/Car/Car/index.tsx
+++ components/Car/Car/index.tsx
@@ -192,6 +192,8 @@ const Car = ({ translations, query }: CarPageProps) => {
           isEditMode={isEditMode}
           ad={selectedAd}
           car={carQuery.data.car}
+          pageIndexAd={pageIndexAds}
+          pageIndexOffer={pageIndexOffer}
           offer={selectedOffer}
         />
       )}
--- components/Offer/OfferModal/index.tsx
+++ components/Offer/OfferModal/index.tsx
@@ -16,7 +16,6 @@ import { CREATE_OFFER_MUTATION, UPDATE_OFFER_MUTATION } from './Mutations';
 import { CAR_BY_ID, MATCHING_ADS_QUERY } from '../../Car/Car/Queries';
 import { OFFER_ADDONS_QUERY } from './Queries';
 import { paging10pages } from '../../General/Preferences';
-import Router from 'next/router';
 
 interface OfferModalProps {
   translations: Translations;
@@ -24,6 +23,8 @@ interface OfferModalProps {
   isEditMode: boolean;
   ad: Ad;
   car: Car;
+  pageIndexAd: number;
+  pageIndexOffer: number;
   offer: Offer;
   toggleModal: (...params: any) => void;
 }
@@ -35,6 +36,8 @@ const OfferModal = ({
   isEditMode,
   ad,
   car,
+  pageIndexAd,
+  pageIndexOffer,
   offer,
 }: OfferModalProps) => {
   const { data, loading } = useQuery(OFFER_ADDONS_QUERY);
@@ -54,7 +57,7 @@ const OfferModal = ({
         query: CAR_BY_ID,
         variables: {
           id: car.id,
-          pageNumberOffer: 0,
+          pageNumberOffer: pageIndexOffer,
           pageSizeOffer: paging10pages,
         },
       },
@@ -62,7 +65,7 @@ const OfferModal = ({
         query: MATCHING_ADS_QUERY,
         variables: {
           id: car.id,
-          pageNumberOffer: 0,
+          pageNumberOffer: pageIndexAd,
           pageSizeOffer: paging10pages,
         },
       },
@@ -71,7 +74,16 @@ const OfferModal = ({
 
   const handleUpdateOffer = useMutation(UPDATE_OFFER_MUTATION, {
     variables: getUpdateOfferPayload(),
-    refetchQueries: [{ query: CAR_BY_ID, variables: { id: car.id } }],
+    refetchQueries: [
+      {
+        query: CAR_BY_ID,
+        variables: {
+          id: car.id,
+          pageNumberOffer: 0,
+          pageSizeOffer: paging10pages,
+        },
+      },
+    ],
   });
 
   function getCreateOfferPayload() {
