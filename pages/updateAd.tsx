b3ed921fbd39079fee46032c6255025e0283c2ee
Alexandre Clark <alex.clark.015@gmail.com>
2019-04-14 13:32:36 -0400

    Redirect when not the user ad

--- pages/updateAd.tsx
+++ pages/updateAd.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import UpdateAd from '../components/Ad/UpdateAd';
 import PrivateComponent from '../lib/Auth/PrivateComponent';
+import checkAdExists from '../lib/PostExists/checkAdExists';
+import redirect from '../lib/Auth/Redirect';
 
 class UpdateAdPage extends PrivateComponent {
+  static async getInitialProps(ctx: any) {
+    const { user } = await PrivateComponent.getInitialProps(ctx);
+    const ad = await checkAdExists(ctx.apolloClient, ctx.query.id);
+
+    if (!ad || !ad.creator || ad.creator.id !== user.id) {
+      redirect(ctx, '/login');
+    }
+    return { ad, user };
+  }
   render() {
     return (
       <div>
